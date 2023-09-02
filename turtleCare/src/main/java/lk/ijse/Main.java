fee121bd9a0bfc390ff418cb302a502eeae79bf9
kavindumal <kavindu11250403@gmail.com>
2023-09-02 12:57:05 +0530

    test commit

--- turtleCare/src/main/java/lk/ijse/Main.java
+++ turtleCare/src/main/java/lk/ijse/Main.java
@@ -2,11 +2,6 @@ package lk.ijse;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
-        System.out.println("dxnhgv");
-        System.out.println("hdgfhg");
-        System.out.println("thth");
-        System.out.println("fghghd");
-        System.out.println("ghfchgf");
+        System.out.println("hello");
     }
 }
