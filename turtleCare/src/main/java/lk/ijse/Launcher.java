2bdb2f0dd0ff543dddfa017ae958ecb075865c6a
kavindumal <kavindu11250403@gmail.com>
2023-09-02 19:56:34 +0530

    login page

--- turtleCare/src/main/java/lk/ijse/Launcher.java
+++ turtleCare/src/main/java/lk/ijse/Launcher.java
@@ -1,4 +1,25 @@
 package lk.ijse;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/loginForm.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        stage.setTitle("Login page");
+        stage.centerOnScreen();
+        stage.setScene(scene);
+
+        stage.show();
+    }
 }
