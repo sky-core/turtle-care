50fc6a6fe96d6db9470bd2bdf1d2f3564ff9fd23
kavindumal <kavindu11250403@gmail.com>
2023-09-04 14:14:17 +0530

    DB editing

--- turtleCare/src/main/java/lk/ijse/controller/DashboardFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/DashboardFormController.java
@@ -1,4 +1,30 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+
 public class DashboardFormController {
+    @FXML
+    private JFXButton btn1;
+
+    @FXML
+    void onMouseEntere(MouseEvent event) {
+        btn1.setBackground(Background.fill(Color.GREEN));
+    }
+
+    @FXML
+    void onMouseExited(MouseEvent event) {
+
+    }
+
+    public void onMouseEntere(javafx.scene.input.MouseEvent mouseEvent) {
+    }
+
+    public void onMouseExited(javafx.scene.input.MouseEvent mouseEvent) {
+    }
 }
