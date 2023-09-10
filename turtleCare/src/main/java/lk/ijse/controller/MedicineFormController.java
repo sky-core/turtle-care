5134779db2e9cb294e05f0814a36549a0c1d2d93
kavindumal <kavindu11250403@gmail.com>
2023-09-10 14:04:27 +0530

    forms creating

--- turtleCare/src/main/java/lk/ijse/controller/MedicineFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/MedicineFormController.java
@@ -1,7 +1,11 @@
 package lk.ijse.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class MedicineFormController {
+
     @FXML
+    private AnchorPane medicinePane;
+
 }
