01e0c7c14acdd0d7b1aa6f127d701687063328cc
kavindumal <kavindu11250403@gmail.com>
2023-09-10 11:57:35 +0530

    actionLogin

--- turtleCare/src/main/java/lk/ijse/controller/LoginFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/LoginFormController.java
@@ -4,10 +4,14 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class LoginFormController{
     @FXML
     private JFXButton loginButton;
+
+    @FXML
+    private AnchorPane lognPage;
 
     @FXML
     void login(ActionEvent event) {
