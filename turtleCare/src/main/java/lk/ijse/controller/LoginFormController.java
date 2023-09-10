d3440eadffba63a6ce6a225f435fee600624b35d
kavindumal <kavindu11250403@gmail.com>
2023-09-10 11:54:05 +0530

    actionLogin

--- turtleCare/src/main/java/lk/ijse/controller/LoginFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,7 +1,16 @@
 package lk.ijse.controller;
 
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginFormController{
+    @FXML
+    private JFXButton loginButton;
 
+    @FXML
+    void login(ActionEvent event) {
 
+    }
 }
