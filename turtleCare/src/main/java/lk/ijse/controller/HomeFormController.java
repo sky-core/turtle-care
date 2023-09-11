c93b8fdb18ab8d72cd8bd9d8ab14b267449f2ddb
kavindumal <kavindu11250403@gmail.com>
2023-09-11 08:58:22 +0530

    homeForm.fxml finished

--- turtleCare/src/main/java/lk/ijse/controller/HomeFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/HomeFormController.java
@@ -2,6 +2,7 @@ package lk.ijse.controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class HomeFormController {
@@ -10,6 +11,42 @@ public class HomeFormController {
     private JFXButton changeHeat;
 
     @FXML
+    private Label daysForHatch;
+
+    @FXML
+    private Label divisionNumber;
+
+    @FXML
+    private Label donName1;
+
+    @FXML
+    private Label donName2;
+
+    @FXML
+    private Label donPrice1;
+
+    @FXML
+    private Label donPrice2;
+
+    @FXML
+    private Label eggRoomCelcius;
+
+    @FXML
     private AnchorPane homePane;
+
+    @FXML
+    private Label inShowRoom;
+
+    @FXML
+    private Label spaceShowRoom;
+
+    @FXML
+    private Label todCusCount;
+
+    @FXML
+    private Label todIncome;
+
+    @FXML
+    private Label viewMoreDon;
 
 }
