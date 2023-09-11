51b7c8a9a3a145023a7084c3eaaea16d10fc0f55
kavindumal <kavindu11250403@gmail.com>
2023-09-11 17:28:14 +0530

    employeeNameAndDetailsForm.fxml finished

---
+++ turtleCare/src/main/java/lk/ijse/controller/EmployeeNameAndDetailsFormController.java
@@ -0,0 +1,30 @@
+package lk.ijse.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class EmployeeNameAndDetailsFormController {
+
+    @FXML
+    private ImageView btnBack;
+
+    @FXML
+    private TextField empAdd;
+
+    @FXML
+    private TextField empDOB;
+
+    @FXML
+    private TextField empName;
+
+    @FXML
+    private TextField empPN;
+
+    @FXML
+    private TextField empWT;
+
+    @FXML
+    private TextField joinedDate;
+
+}
