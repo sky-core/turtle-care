1fcfb4342bdeb75a8f75e406f2b6337c4bd0fb8f
kavindumal <kavindu11250403@gmail.com>
2023-09-11 15:44:42 +0530

    settingForm.fxml finished

--- turtleCare/src/main/java/lk/ijse/controller/SettingFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/SettingFormController.java
@@ -1,10 +1,22 @@
 package lk.ijse.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.Launcher;
+import lk.ijse.LauncherWrapper;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class SettingFormController {
 
@@ -46,5 +58,22 @@ public class SettingFormController {
 
     @FXML
     private TextField userName;
+
+    @FXML
+    void logOutBtnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) logOutbtn.getScene().getWindow();
+        stage.close();
+
+        Parent rootNode = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/loginForm.fxml")));
+
+        Scene scene = new Scene(rootNode);
+
+        stage.setTitle("Login page");
+        stage.centerOnScreen();
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
 
 }
