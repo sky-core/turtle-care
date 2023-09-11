cb041cca477c5d60de6deb891d79ce50eab0e000
kavindumal <kavindu11250403@gmail.com>
2023-09-11 11:20:48 +0530

    settingForm.fxml finished

--- turtleCare/src/main/java/lk/ijse/controller/SettingFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/SettingFormController.java
@@ -1,11 +1,50 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class SettingFormController {
 
     @FXML
+    private JFXButton addAccountBtn;
+
+    @FXML
+    private TextField conEmailAdd;
+
+    @FXML
+    private TextField confirmPwSet;
+
+    @FXML
+    private JFXButton delAccountBtn;
+
+    @FXML
+    private TextField emailAdd;
+
+    @FXML
+    private TextField fullName;
+
+    @FXML
+    private JFXButton logOutbtn;
+
+    @FXML
+    private Label nameOfProfile;
+
+    @FXML
+    private TextField pwSet;
+
+    @FXML
     private AnchorPane settingPane;
+
+    @FXML
+    private JFXButton upNewPhoto;
+
+    @FXML
+    private JFXButton updInfobtn;
+
+    @FXML
+    private TextField userName;
 
 }
