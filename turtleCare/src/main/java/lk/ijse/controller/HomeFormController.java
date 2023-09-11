a2071332572961a47156b0aeac7f4ddaa75f6853
kavindumal <kavindu11250403@gmail.com>
2023-09-11 08:47:20 +0530

    forms creating

--- turtleCare/src/main/java/lk/ijse/controller/HomeFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/HomeFormController.java
@@ -1,9 +1,13 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
 public class HomeFormController {
+
+    @FXML
+    private JFXButton changeHeat;
 
     @FXML
     private AnchorPane homePane;
