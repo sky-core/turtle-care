1c2571a526a2313ab264a4154e054f1c00dc595f
kavindumal <kavindu11250403@gmail.com>
2023-09-11 16:39:51 +0530

    turtleForm.fxml finished

--- turtleCare/src/main/java/lk/ijse/controller/TurtleFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/TurtleFormController.java
@@ -1,9 +1,32 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class TurtleFormController {
+
+    @FXML
+    private JFXButton btnAddFood;
+
+    @FXML
+    private JFXButton btnLTShowMore;
+
+    @FXML
+    private JFXButton btnMSShowMore;
+
+    @FXML
+    private JFXButton btnTPShowMore;
+
+    @FXML
+    private Label labelFoodStock;
+
+    @FXML
+    private Label lblLittleTurtle;
+
+    @FXML
+    private Label lblTurtlePatoent;
 
     @FXML
     private AnchorPane turtlePane;
