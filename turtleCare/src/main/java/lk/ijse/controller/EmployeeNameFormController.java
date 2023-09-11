0c1514f434e68943f4e23a9740fce8f490358594
kavindumal <kavindu11250403@gmail.com>
2023-09-11 17:09:29 +0530

    employeeNameForm.fxml finished

--- turtleCare/src/main/java/lk/ijse/controller/EmployeeNameFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/EmployeeNameFormController.java
@@ -1,9 +1,188 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class EmployeeNameFormController {
+
+    @FXML
+    private JFXButton btnAddEmployee;
+
+    @FXML
+    private JFXButton btnRemoveEmployee;
+
+    @FXML
+    private JFXButton btnUpdateEmployee;
+
+    @FXML
+    private Label empId1;
+
+    @FXML
+    private Label empId10;
+
+    @FXML
+    private Label empId11;
+
+    @FXML
+    private Label empId12;
+
+    @FXML
+    private Label empId13;
+
+    @FXML
+    private Label empId14;
+
+    @FXML
+    private Label empId15;
+
+    @FXML
+    private Label empId16;
+
+    @FXML
+    private Label empId17;
+
+    @FXML
+    private Label empId18;
+
+    @FXML
+    private Label empId19;
+
+    @FXML
+    private Label empId2;
+
+    @FXML
+    private Label empId20;
+
+    @FXML
+    private Label empId21;
+
+    @FXML
+    private Label empId22;
+
+    @FXML
+    private Label empId23;
+
+    @FXML
+    private Label empId24;
+
+    @FXML
+    private Label empId25;
+
+    @FXML
+    private Label empId26;
+
+    @FXML
+    private Label empId27;
+
+    @FXML
+    private Label empId28;
+
+    @FXML
+    private Label empId3;
+
+    @FXML
+    private Label empId4;
+
+    @FXML
+    private Label empId5;
+
+    @FXML
+    private Label empId6;
+
+    @FXML
+    private Label empId7;
+
+    @FXML
+    private Label empId8;
+
+    @FXML
+    private Label empId9;
+
+    @FXML
+    private Label empName1;
+
+    @FXML
+    private Label empName10;
+
+    @FXML
+    private Label empName11;
+
+    @FXML
+    private Label empName12;
+
+    @FXML
+    private Label empName13;
+
+    @FXML
+    private Label empName14;
+
+    @FXML
+    private Label empName15;
+
+    @FXML
+    private Label empName16;
+
+    @FXML
+    private Label empName17;
+
+    @FXML
+    private Label empName18;
+
+    @FXML
+    private Label empName19;
+
+    @FXML
+    private Label empName2;
+
+    @FXML
+    private Label empName20;
+
+    @FXML
+    private Label empName21;
+
+    @FXML
+    private Label empName22;
+
+    @FXML
+    private Label empName23;
+
+    @FXML
+    private Label empName24;
+
+    @FXML
+    private Label empName25;
+
+    @FXML
+    private Label empName26;
+
+    @FXML
+    private Label empName27;
+
+    @FXML
+    private Label empName28;
+
+    @FXML
+    private Label empName3;
+
+    @FXML
+    private Label empName4;
+
+    @FXML
+    private Label empName5;
+
+    @FXML
+    private Label empName6;
+
+    @FXML
+    private Label empName7;
+
+    @FXML
+    private Label empName8;
+
+    @FXML
+    private Label empName9;
 
     @FXML
     private AnchorPane employeeNamePane;
