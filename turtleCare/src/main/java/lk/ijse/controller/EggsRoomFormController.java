4963982eaacdb7bb943fb32439b06b2114c78f2c
kavindumal <kavindu11250403@gmail.com>
2023-09-11 16:11:40 +0530

    eggsRoomForm.fxml finished

--- turtleCare/src/main/java/lk/ijse/controller/EggsRoomFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/EggsRoomFormController.java
@@ -1,11 +1,28 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXSlider;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class EggsRoomFormController {
 
     @FXML
+    private Label divisionOfHatch;
+
+    @FXML
+    private Label eggsInStock;
+
+    @FXML
     private AnchorPane eggsRoomPane;
+
+    @FXML
+    private Label numberOfDaysForHatch;
+
+    @FXML
+    private JFXSlider sliderOfControlTemp;
+
+    @FXML
+    private Label temperatureShowLbl;
 
 }
