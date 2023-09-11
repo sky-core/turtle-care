3e8e49c3089c5f7d31cbc4f377ca17a8f7b7ffee
kavindumal <kavindu11250403@gmail.com>
2023-09-11 10:32:18 +0530

    employeeDetailsForm.fxml finished

--- turtleCare/src/main/java/lk/ijse/controller/EmployeeDetailsFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/EmployeeDetailsFormController.java
@@ -1,11 +1,45 @@
 package lk.ijse.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class EmployeeDetailsFormController {
 
     @FXML
     private AnchorPane employeePane;
+
+    @FXML
+    private Label todWorkEmpid1;
+
+    @FXML
+    private Label todWorkEmpid10;
+
+    @FXML
+    private Label todWorkEmpid11;
+
+    @FXML
+    private Label todWorkEmpid2;
+
+    @FXML
+    private Label todWorkEmpid3;
+
+    @FXML
+    private Label todWorkEmpid4;
+
+    @FXML
+    private Label todWorkEmpid5;
+
+    @FXML
+    private Label todWorkEmpid6;
+
+    @FXML
+    private Label todWorkEmpid7;
+
+    @FXML
+    private Label todWorkEmpid8;
+
+    @FXML
+    private Label todWorkEmpid9;
 
 }
--- turtleCare/src/main/java/lk/ijse/controller/TicketPageFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/TicketPageFormController.java
@@ -1,9 +1,21 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class TicketPageFormController {
+
+    @FXML
+    private Label earningsFromTicket;
+
+    @FXML
+    private BarChart<?, ?> lMonthTIcketsIssues;
+
+    @FXML
+    private JFXButton newDon;
 
     @FXML
     private AnchorPane ticketsPane;
