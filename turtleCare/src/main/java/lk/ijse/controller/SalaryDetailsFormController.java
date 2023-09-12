392e00265d29d789c32535a76114692db4488314
kavindumal <kavindu11250403@gmail.com>
2023-09-12 10:55:28 +0530

    salaryDetailsForm.fxml finished

--- turtleCare/src/main/java/lk/ijse/controller/SalaryDetailsFormController.java
+++ turtleCare/src/main/java/lk/ijse/controller/SalaryDetailsFormController.java
@@ -1,11 +1,26 @@
 package lk.ijse.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class SalaryDetailsFormController {
 
     @FXML
+    private Pane mo;
+
+    @FXML
     private AnchorPane salaryDetailsPane;
+
+    @FXML
+    void btnBackOnAction(MouseEvent event) throws IOException {
+        salaryDetailsPane.getChildren().clear();
+        salaryDetailsPane.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/employeeDetailsForm.fxml"))));
+    }
 
 }
